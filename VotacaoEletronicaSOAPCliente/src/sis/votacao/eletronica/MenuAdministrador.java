8cc72e5871ad4d409f5e3001c64724947a7579d3
MMonteiro <56174460+MMonteiro-21801093@users.noreply.github.com>
2021-01-17 01:26:38 +0000

    Add files via upload

--- VotacaoEletronicaSOAPCliente/src/sis/votacao/eletronica/MenuAdministrador.java
+++ VotacaoEletronicaSOAPCliente/src/sis/votacao/eletronica/MenuAdministrador.java
@@ -13,125 +13,150 @@ import javax.xml.namespace.QName;
 import votacao.eletronica.*;
  
 public class MenuAdministrador {
-    private static final QName SERVICE_NAME = new QName("http://eletronica.votacao/", "VotacaoSoapWSService");
-    static URL wsdlURL = VotacaoSoapWSService.WSDL_LOCATION;
-    static VotacaoSoapWSService ss = new VotacaoSoapWSService(wsdlURL, SERVICE_NAME);
-    static   VotacaoSoapWS port = ss.getVotacaoSoapWSPort(); 
+	 
+    static   VotacaoSoapWS port;
+ 
+	private static void connectWS() {
+		try {
+	     QName SERVICE_NAME = new QName("http://eletronica.votacao/", "VotacaoSoapWSService");
+	     URL wsdlURL = VotacaoSoapWSService.WSDL_LOCATION;
+ 
+	     VotacaoSoapWSService ss = new VotacaoSoapWSService(wsdlURL, SERVICE_NAME);
+	      port = ss.getVotacaoSoapWSPort(); 
+		} catch (Exception e) {
+	        try {
+				Thread.sleep(5000);
+				connectWS();
+			} catch (Exception e1) {
+
+			}
+	     
+		}
+		
+ 
+}
     public static void main(String[] args)   throws java.lang.Exception {
-    
+        connectWS();
 		String user_id = ""; // user id
 		String user_pwd = ""; // user password
 		String base = "ou=prod,dc=example,dc=com";
 		String user_rdn ="ou=admin"; // user RDN (relative distinguished names)
 		String dn = "uid=" + user_id + "," + user_rdn + "," + base; // DN (distinguished name)
 		String opcaoSelecionada ="";
+	    Scanner scan = new Scanner(System.in);
+        String valida="";
  
-	       Scanner scan = new Scanner(System.in);
-		   System.out.println("login:");
-		     user_id = scan.nextLine();  // Read user input
-		    System.out.println("password:");
-              user_pwd = scan.nextLine();  // Read user input
-		     dn = "uid=" + user_id + "," + user_rdn + "," + base; 
+	       do {	   
+			      System.out.println("login:");
+			      user_id = scan.nextLine();  // Read user input
+			      System.out.println("password:");
+	              user_pwd = scan.nextLine();  // Read user input
+			      dn = "uid=" + user_id + "," + user_rdn + "," + base; 
+			      valida = port.validaUtilizador(dn, user_pwd); 
+                  if(!valida.equals("OK")) {
+	                  System.out.println(port.validaUtilizador(dn, user_pwd));
+	              }
+		      }while(!valida.equals("OK"));     
 		
-        do {
-			if (port.validaUtilizador(dn, user_pwd).equals("OK")) {
-				System.out.println(" ");
-				System.out.println("MENU ADMINISTRADOR");
-				System.out.println("0 - Menu inicial");
-				System.out.println("1 - Listar itens em votação");
-				System.out.println("2 - Início da sessão");
-				System.out.println("3 - Duração da sessão");
-				System.out.println("4 - Tempo restante de votação");
-				System.out.println("5 - Número total de votos");
-				System.out.println("6 - Listar resultados de votação (%)");
-				System.out.println("7 - Item ganhador");
-				System.out.println("8 - listar utilizadores registados");
-				System.out.println("9 - listar utilizadores da sessão");
-				System.out.println("10 - associar utilizador");
-				System.out.println("11 - remover utilizador");
-				System.out.println("99 - Sair");
-			    opcaoSelecionada = scan.nextLine();
-	
+    
+      	       if(valida.equals("OK")) {	
+      	             mostraMenu();
+	                opcaoSelecionada = scan.nextLine(); 
+ 
+	          do {
 				switch(opcaoSelecionada) {
+				case "0":
+				  mostraMenu();
+			     opcaoSelecionada = scan.nextLine();
+		        break;
 				case "1":
 				listaItemsVotacao();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 				break;
 				case "2":
 			    inicioDaVotacao();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "3":
 			  	duracaoDataSessao();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "4":
 			    tempoRestanteSessao();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "5":
 			     numeroTotalVotos();
-			 	System.out.println("Prima ENTER para continuar");
+			 	System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "6":
 			    listarResultadosVotacao();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "7":
 			    itemGanhador();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "8":
 			    listaUtilizadoresRegistados();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "9":
 			    listaUtilizadoresSessao();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "10":
 		        associarUtilizador();
-		    	System.out.println("Prima ENTER para continuar");
+		    	System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 			    break;
 				case "11":
 			    removerUtilizador();
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
-			    break;
-				case "99":
-			    System.out.println("A sair");  
-			    System.out.println("Cliente Desconectado..");   
 			    break;
 			    default:
 			    System.out.println("Opção inválida");
-				System.out.println("Prima ENTER para continuar");
+				System.out.println("Opção?");
 			    opcaoSelecionada = scan.nextLine();
 				}
-			}	
-			else {
-				System.out.println("Autenticação inválida");
-				System.out.println(" ");
-			    scan = new Scanner(System.in);
-			    System.out.println("login:");
-			    user_id = scan.nextLine();  // Read user input
-			    System.out.println("password:");
-		        user_pwd = scan.nextLine();  // Read user input
-			    dn = "uid=" + user_id + "," + user_rdn + "," + base; 
-	 
-			} 
+ 
 			
-        }while(!opcaoSelecionada.equals("99"));
+               }while(!opcaoSelecionada.equals("99"));
+      	       }
+      	       
+      		  if(opcaoSelecionada.equals("99")) {
+			      System.out.println("A sair");  
+			       System.out.println("Cliente Desconectado..");  
+				  }
     }
+	private static void mostraMenu() {
+		System.out.println(" ");
+		System.out.println("MENU ADMINISTRADOR");
+		System.out.println("0 - Menu inicial");
+		System.out.println("1 - Listar itens em votação");
+		System.out.println("2 - Início da sessão");
+		System.out.println("3 - Duração da sessão");
+		System.out.println("4 - Tempo restante de votação");
+		System.out.println("5 - Número total de votos");
+		System.out.println("6 - Listar resultados de votação (%)");
+		System.out.println("7 - Item ganhador");
+		System.out.println("8 - listar utilizadores registados");
+		System.out.println("9 - listar utilizadores da sessão");
+		System.out.println("10 - associar utilizador");
+		System.out.println("11 - remover utilizador");
+		System.out.println("99 - Sair");
+		
+	}
 	private static void listaItemsVotacao() throws Exception_Exception {
 	 
 			System.out.println(port.listaItemsVotacao());
--- VotacaoEletronicaSOAPCliente/src/sis/votacao/eletronica/MenuVotante.java
+++ VotacaoEletronicaSOAPCliente/src/sis/votacao/eletronica/MenuVotante.java
@@ -9,103 +9,129 @@ import javax.xml.namespace.QName;
 import votacao.eletronica.Exception_Exception;
 import votacao.eletronica.VotacaoSoapWS;
 import votacao.eletronica.VotacaoSoapWSService;
-
+ 
 public class MenuVotante {
-    private static final QName SERVICE_NAME = new QName("http://eletronica.votacao/", "VotacaoSoapWSService");
-    static URL wsdlURL = VotacaoSoapWSService.WSDL_LOCATION;
-    static VotacaoSoapWSService ss = new VotacaoSoapWSService(wsdlURL, SERVICE_NAME);
-    static   VotacaoSoapWS port = ss.getVotacaoSoapWSPort(); 
+ 
+    static   VotacaoSoapWS port;
+ 
+	private static void connectWS() {
+		try {
+	     QName SERVICE_NAME = new QName("http://eletronica.votacao/", "VotacaoSoapWSService");
+	     URL wsdlURL = VotacaoSoapWSService.WSDL_LOCATION;
+ 
+	     VotacaoSoapWSService ss = new VotacaoSoapWSService(wsdlURL, SERVICE_NAME);
+	      port = ss.getVotacaoSoapWSPort(); 
+		} catch (Exception e) {
+	        try {
+				Thread.sleep(5000);
+				connectWS();
+			} catch (Exception e1) {
+
+			}
+	     
+		}
+		
+ 
+}
 	public static void main(String[] args)  throws java.lang.Exception {
-	  	String teste ="";
+	        connectWS();
+	     	String teste ="";
 			String user_id = ""; // user id
 			String user_pwd = ""; // user password
 			String base = "ou=prod,dc=example,dc=com";
 			String user_rdn ="ou=people"; // user RDN (relative distinguished names)
 			String dn = "uid=" + user_id + "," + user_rdn + "," + base; // DN (distinguished name)
 			String opcaoSelecionada ="";
+	        Scanner scan = new Scanner(System.in);
+	        String valida="";
 	   //2111188
-		       Scanner scan = new Scanner(System.in);
-			   System.out.println("login:");
-			     user_id = scan.nextLine();  // Read user input
-			    System.out.println("password:");
+		
+		      do {
+        	   
+			      System.out.println("login:");
+			      user_id = scan.nextLine();  // Read user input
+			      System.out.println("password:");
 	              user_pwd = scan.nextLine();  // Read user input
-			     dn = "uid=" + user_id + "," + user_rdn + "," + base; 
-
-			     
-			     do {
-					  if (port.validaUtilizador(dn, user_pwd).equals("OK")) {
-							System.out.println(" ");
-							System.out.println("MENU VOTANTE");
-							System.out.println("0 - Menu inicial");
-							System.out.println("1 - Listar itens em votação");
-							System.out.println("2 - Tempo restante da votação");
-							System.out.println("3 - Votar");
-							System.out.println("4 - Número total de votos");
-							System.out.println("5 - Listar resultados de votação (%)");
-							System.out.println("6 - Item ganhador");
-							System.out.println("99 - Sair");
-						    opcaoSelecionada = scan.nextLine();
-				
-							switch(opcaoSelecionada) {
-							case "1":
-								listaItemsVotacao();
-								System.out.println("Prima ENTER para continuar");
-							    opcaoSelecionada = scan.nextLine();
-								break;
-							case "2":
-							    tempoRestanteSessao();
-								System.out.println("Prima ENTER para continuar");
-							    opcaoSelecionada = scan.nextLine();
-								break;
-							case "3":
-								votar(user_id);
-								System.out.println("Prima ENTER para continuar");
-							    opcaoSelecionada = scan.nextLine();
-								break;
-							case "4":
-								numeroTotalVotos();
-								System.out.println("Prima ENTER para continuar");
-							    opcaoSelecionada = scan.nextLine();
-								break;
-							case "5":
-								listarResultadosVotacao();
-								System.out.println("Prima ENTER para continuar");
-							    opcaoSelecionada = scan.nextLine();
-								break;
-							case "6":
-							    itemGanhador();
-							    System.out.println("Prima ENTER para continuar");
-							    opcaoSelecionada = scan.nextLine();
-								break;
-							case "99":
-							    System.out.println("A sair");  
-							    System.out.println("Cliente Desconectado..");  
-								break;
-						    default:
-						    System.out.println("Opção inválida");	
-							System.out.println("Prima ENTER para continuar");
-						    opcaoSelecionada = scan.nextLine();
-							}
-						}	
-						else {
-							System.out.println("Autenticação inválida");
-							System.out.println(" ");
-						    scan = new Scanner(System.in);
-						    System.out.println("login:");
-					        user_id = scan.nextLine();  // Read user input
-						    System.out.println("password:");
-					        user_pwd = scan.nextLine();  // Read user input
-					        dn = "uid=" + user_id + "," + user_rdn + "," + base; 
-						} 
-						
-			        }while(!opcaoSelecionada.equals("99"));
-			     
+			      dn = "uid=" + user_id + "," + user_rdn + "," + base; 
+			      valida = port.validaUtilizador(dn, user_pwd); 
+                if(!valida.equals("OK")) {
+	                System.out.println(port.validaUtilizador(dn, user_pwd));
+	              }
+		      }while(!valida.equals("OK"));     
+ 
+				  if(valida.equals("OK")) {		
+	                mostraMenu();
+	                opcaoSelecionada = scan.nextLine(); 
+			    do {
+				  
+					switch(opcaoSelecionada) {
+					case "0":
+						 mostraMenu();
+					    opcaoSelecionada = scan.nextLine();
+						break;
+					case "1":
+						listaItemsVotacao();
+						System.out.println("Opção?");
+					    opcaoSelecionada = scan.nextLine();
+						break;
+					case "2":
+					    tempoRestanteSessao();
+						System.out.println("Opção?");
+					    opcaoSelecionada = scan.nextLine();
+						break;
+					case "3":
+						votar(user_id);
+						System.out.println("Opção?");
+					    opcaoSelecionada = scan.nextLine();
+						break;
+					case "4":
+						numeroTotalVotos();
+						System.out.println("Opção?");
+					    opcaoSelecionada = scan.nextLine();
+						break;
+					case "5":
+						listarResultadosVotacao();
+						System.out.println("Opção?");
+					    opcaoSelecionada = scan.nextLine();
+						break;
+					case "6":
+					    itemGanhador();
+						System.out.println("Opção?");
+					    opcaoSelecionada = scan.nextLine();
+						break;
+				    default:
+				    System.out.println("Opção inválida");	
+					System.out.println("Opção?");
+				    opcaoSelecionada = scan.nextLine();
+					}
+              }while(!opcaoSelecionada.equals("99"));    
+				  }
+				  
+				  if(opcaoSelecionada.equals("99")) {
+					 System.out.println("A sair");  
+				     System.out.println("Cliente Desconectado..");  
+				  }
+      
+	}
+	private static void mostraMenu() {
+		System.out.println(" ");
+		System.out.println("MENU VOTANTE");
+		System.out.println("0 - Menu inicial");
+		System.out.println("1 - Listar itens em votação");
+		System.out.println("2 - Tempo restante da votação");
+		System.out.println("3 - Votar");
+		System.out.println("4 - Número total de votos");
+		System.out.println("5 - Listar resultados de votação (%)");
+		System.out.println("6 - Item ganhador");
+		System.out.println("99 - Sair");
+		
 	}
 	private static void votar(String user_id) throws Exception {
 	       Scanner scan = new Scanner(System.in);
 	       String opt="";
 	       String codigo="";
 	       String itemVotado ="";
+	       
 	       String tempo = port.tempoRestanteSessao();
 	  
 	       if(tempo.startsWith("Sessão iniciada")) {
@@ -131,9 +157,10 @@ public class MenuVotante {
 	      
 	       
 	}
-	private static void listaItemsVotacao() throws Exception_Exception {
-		System.out.println(port.listaItemsVotacao());
-		
+	private static void listaItemsVotacao() throws Exception{
+	 
+	          	System.out.println(port.listaItemsVotacao());	
+  	 
 	}
 
 	private static void listaUtilizadoresSessao() throws Exception{
